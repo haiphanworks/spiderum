b3813e1359bfb50bdc5e24a85b393bcf15ac7026
Hải Phan <hai@haiphan.design>
2020-05-19 06:40:58 +1000

    Chat System [Interactive]

--- assets/js/app.js
+++ assets/js/app.js
@@ -110,3 +110,158 @@ $('.filter\\:sort .item').click(function (e) {
 	$(this).parent().children().removeClass('is:active');
 	$(this).addClass('is:active');
 });
+
+// Chat Active Switch
+
+$('.global-chat .chat-active .chat-box').click(function (e) {
+	e.preventDefault();
+	var clickTarget = $(e.target);
+	if (
+		clickTarget.closest('.item.hide').length ||
+		clickTarget.closest('.item.close').length
+	) {
+		if (clickTarget.closest('.item.hide').length) {
+			$(this).removeClass('is:closed');
+			$(this).addClass('is:hidden');
+		} else if (clickTarget.closest('.item.close').length) {
+			$(this).removeClass('is:hidden');
+			$(this).addClass('is:closed');
+		}
+	} else {
+		if ($(this).hasClass('is:focus')) {
+		} else {
+			$('.global-chat .chat-active .chat-box').removeClass('is:focus');
+			$(this).addClass('is:focus');
+		}
+	}
+});
+
+// ACTIVE CHAT
+// CHAT 1
+$('.global-chat .chat-active .chat-1 .actions .hide').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-1')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+	$('.global-chat .chat-inactive .chat-1')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-active .chat-1 .actions .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-1')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+	$('.global-chat .chat-inactive .chat-1')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+});
+// CHAT 2
+$('.global-chat .chat-active .chat-2 .actions .hide').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-2')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+	$('.global-chat .chat-inactive .chat-2')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-active .chat-2 .actions .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-2')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+	$('.global-chat .chat-inactive .chat-2')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+});
+// CHAT 3
+$('.global-chat .chat-active .chat-3 .actions .hide').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-3')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+	$('.global-chat .chat-inactive .chat-3')
+		.addClass('is:hidden')
+		.removeClass('is:active')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-active .chat-3 .actions .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-active .chat-3')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+	$('.global-chat .chat-inactive .chat-3')
+		.addClass('is:closed')
+		.removeClass('is:active')
+		.removeClass('is:hidden');
+});
+
+// INACTIVE CHAT
+// CHAT 1
+$('.global-chat .chat-inactive .chat-1 .avatar').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-1')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+	$('.global-chat .chat-active .chat-1')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-inactive .chat-1 .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-1')
+		.addClass('is:closed')
+		.removeClass('is:hidden')
+		.removeClass('is:active');
+});
+// CHAT 2
+$('.global-chat .chat-inactive .chat-2 .avatar').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-2')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+	$('.global-chat .chat-active .chat-2')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-inactive .chat-2 .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-2')
+		.addClass('is:closed')
+		.removeClass('is:hidden')
+		.removeClass('is:active');
+});
+// CHAT 3
+$('.global-chat .chat-inactive .chat-3 .avatar').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-3')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+	$('.global-chat .chat-active .chat-3')
+		.addClass('is:active')
+		.removeClass('is:hidden')
+		.removeClass('is:closed');
+});
+$('.global-chat .chat-inactive .chat-3 .close').click(function (e) {
+	e.preventDefault();
+	$('.global-chat .chat-inactive .chat-3')
+		.addClass('is:closed')
+		.removeClass('is:hidden')
+		.removeClass('is:active');
+});
